dd98718b51f09f702130e0c61bbc989bb0a2c0a1
suttsc <sutts_c@yahoo.co.uk>
2021-12-02 19:36:51 +0000

    tidy

--- day0.php
+++ day0.php
@@ -1,24 +1,17 @@
 <?php
 
-$day = 1;
+$day = 0;
 $testInput = getInput($day, true);
 $input = getInput($day);
 
 function partOne(array $input)
 {
-    $previous = array_shift($input);
-    $count = 0;
-    foreach ($input as $next) {
-        if($next > $previous) $count++;
-        $previous = $next;
-    }
 
-    return $count;
 }
 
 $partOneTestOutput = partOne($testInput);
 
-echo 'Part one test expected: 7' . '<br/>';
+echo 'Part one test expected: X' . '<br/>';
 echo 'Part one test output: ' . $partOneTestOutput . '<br/>';
 echo '<br/>';
 
@@ -29,20 +22,12 @@ echo '<br/>';
 
 function partTwo(array $input)
 {
-    $previous = array_sum(array_slice($input, 0, 3));
-    $count = 0;
-    for ($i = 1; $i < count($input) - 2; $i++) {
-        $next = array_sum(array_slice($input, $i, 3));
-        if($next > $previous) $count++;
-        $previous = $next;
-    }
 
-    return $count;
 }
 
 $partTwoTestOutput = partTwo($testInput);
 
-echo 'Part two test expected: 5' . '<br/>';
+echo 'Part two test expected: X' . '<br/>';
 echo 'Part two test output: ' . $partTwoTestOutput . '<br/>';
 echo '<br/>';
 
